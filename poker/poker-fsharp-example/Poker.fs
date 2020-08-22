5137fdde80f8b1d125212c948c5eab86cfa35f4d
jbtule <jay+code@tuley.name>
2020-08-22 13:04:28 -0500

    Refactored Type Name in F#

--- poker/poker-fsharp-example/Poker.fs
+++ poker/poker-fsharp-example/Poker.fs
@@ -5,10 +5,10 @@ exception ParseErrorException of string
 type Suit = Heart | Diamond | Club | Spade
 type Card = {Rank:int; Suit:Suit}
 type Hand = Card seq
-type ScoreHand = EntireHand of Hand
-                | SplitHand of Hand * Hand
-                | TwoHand of Hand * Hand * Card
-                | NoHand of Hand
+type ScorableHand = EntireHand of Hand
+                    | SplitHand of Hand * Hand
+                    | TwoHand of Hand * Hand * Card
+                    | NoHand of Hand
 
 let key, groups = fst, snd //aliased for readablity with groupBy
 [<Literal>] 
